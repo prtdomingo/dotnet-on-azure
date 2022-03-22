4ec8c6bb745687a742cb62f49020ccf23337505e
Philip Domingo <phdomingo@microsoft.com>
2022-03-23 01:10:59 +0800

    Updated Index file

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ namespace dotnet_on_azure.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private static string _apiKey = "c591b103-7c27-4c63-856a-b7ea11f89b59";
 
     public IndexModel(ILogger<IndexModel> logger)
     {
